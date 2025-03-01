d39d518c1021e644c7033b9ef05992473d967180
asday <sara.and.zuka@gmail.com>
2025-03-01 01:44:39 +0000

    WIP

--- src/servers/sql/repo.cpp
+++ src/servers/sql/repo.cpp
@@ -1,55 +1,69 @@
+#include <libpq-fe.h>
+
 namespace repo {
   // Registers this launcher with the database for service discovery.
-  const auto sql{"INSERT INTO launcher (ip, port, heartbeat)
-VALUES (inet_client_addr(), $1, NOW()::timestamp)
-RETURNING id"};
-  PGresult* r{PQexecParams(
-    conn,
-    sql,
-    0,
-    nullptr,
-    nullptr,
-    nullptr,
-    nullptr,
-    1
-  )};
-  const ExecStatusType s{PQresultStatus(r)};
-  if (s != PGRES_TUPLES_OK){
-    return std::unexpected(PQerrorMessage(conn));
+  const std::expected<uuid, std::string_view> register() noexcept {
+    const auto sql{
+      "INSERT INTO launcher (ip, port, heartbeat) "
+      "VALUES (inet_client_addr(), $1, NOW()::timestamp) "
+      "RETURNING id "
+    };
+    PGresult* r{PQexecParams(
+      conn,
+      sql,
+      0,
+      nullptr,
+      nullptr,
+      nullptr,
+      nullptr,
+      1
+    )};
+    const ExecStatusType s{PQresultStatus(r)};
+    if (s != PGRES_TUPLES_OK){
+      return std::unexpected(PQerrorMessage(conn));
+    }
   }
   // Pings the database as a service discovery health check.
-  const auto sql{"UPDATE launcher
-SET heartbeat = NOW()::timestamp
-WHERE id = $1"};
-  PGresult* r{PQexecParams(
-    conn,
-    sql,
-    0,
-    nullptr,
-    nullptr,
-    nullptr,
-    nullptr,
-    1
-  )};
-  const ExecStatusType s{PQresultStatus(r)};
-  if (s != PGRES_COMMAND_OK){
-    return std::unexpected(PQerrorMessage(conn));
+  const std::expected<void, std::string_view> heartbeat() noexcept {
+    const auto sql{
+      "UPDATE launcher "
+      "SET heartbeat = NOW()::timestamp "
+      "WHERE id = $1 "
+    };
+    PGresult* r{PQexecParams(
+      conn,
+      sql,
+      0,
+      nullptr,
+      nullptr,
+      nullptr,
+      nullptr,
+      1
+    )};
+    const ExecStatusType s{PQresultStatus(r)};
+    if (s != PGRES_COMMAND_OK){
+      return std::unexpected(PQerrorMessage(conn));
+    }
   }
   // Removes this launcher from the service discovery table.
-  const auto sql{"DELETE FROM launcher
-WHERE id = $1"};
-  PGresult* r{PQexecParams(
-    conn,
-    sql,
-    0,
-    nullptr,
-    nullptr,
-    nullptr,
-    nullptr,
-    1
-  )};
-  const ExecStatusType s{PQresultStatus(r)};
-  if (s != PGRES_COMMAND_OK){
-    return std::unexpected(PQerrorMessage(conn));
+  const std::expected<void, std::string_view> unregister() noexcept {
+    const auto sql{
+      "DELETE FROM launcher "
+      "WHERE id = $1 "
+    };
+    PGresult* r{PQexecParams(
+      conn,
+      sql,
+      0,
+      nullptr,
+      nullptr,
+      nullptr,
+      nullptr,
+      1
+    )};
+    const ExecStatusType s{PQresultStatus(r)};
+    if (s != PGRES_COMMAND_OK){
+      return std::unexpected(PQerrorMessage(conn));
+    }
   }
 }
