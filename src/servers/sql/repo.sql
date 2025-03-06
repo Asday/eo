de57726e5f97af625e7f7e9f60f51928b280d508
asday <sara.and.zuka@gmail.com>
2025-03-06 22:28:05 +0000

    Rudimentary beginnings of a C++ template for `sqlc`.

---
+++ src/servers/sql/repo.sql
@@ -0,0 +1,24 @@
+-- name: register :one
+-- Registers this launcher with the database for service discovery.
+INSERT INTO launcher (ip, port, heartbeat)
+VALUES (inet_client_addr(), sqlc.arg(port), NOW()::timestamp)
+RETURNING id
+;
+
+-- name: heartbeat :exec
+-- Pings the database as a service discovery health check.
+UPDATE launcher
+SET heartbeat = NOW()::timestamp
+WHERE id = sqlc.arg(id)
+;
+
+-- name: unregister :exec
+-- Removes this launcher from the service discovery table.
+DELETE FROM launcher
+WHERE id = sqlc.arg(id)
+;
+
+-- name: launchers :many
+SELECT *
+FROM launcher
+;
